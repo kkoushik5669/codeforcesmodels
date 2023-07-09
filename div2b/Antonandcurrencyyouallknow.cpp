6bcb977007b5da9d8a53e9f4b42beb556808f1ea
kkoushik5669 <118590056+kkoushik5669@users.noreply.github.com>
2023-07-09 14:30:15 +0530

    Create Antonandcurrencyyouallknow.cpp

---
+++ div2b/Antonandcurrencyyouallknow.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+string s;
+int i, k, n, p, m = 9;
+int main()
+{
+cin>>s;
+k = s.size() - 1;
+for (; i <= k; i++)
+if ((s[i] - 48) % 2 == 0)
+{
+p = i;
+m = s[i] - 48;
+if (m < s[k] - 48)
+break;
+}
+s[p] = s[k];
+s[k] = m + 48;
+cout<<(m == 9 ? "-1" : s);
+}
