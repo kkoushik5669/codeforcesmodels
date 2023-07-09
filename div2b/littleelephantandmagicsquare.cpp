58dcb202d146e9ee7ae51ac12847c6d0426e99b6
kkoushik5669 <118590056+kkoushik5669@users.noreply.github.com>
2023-07-09 15:55:17 +0530

    Create littleelephantandmagicsquare.cpp

---
+++ div2b/littleelephantandmagicsquare.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+long a,b,c,d,e,f,g,h,i;
+cin>>a>>b>>c>>d>>e>>f>>g>>h>>i;
+cout<<(f+h)/2<<" "<<b<<" "<<c<<endl<<d<<" "<<(d+f)/2<<" "<<f<<endl<<g<<" "<<h<<" "<<(b+d)/2;
+}
